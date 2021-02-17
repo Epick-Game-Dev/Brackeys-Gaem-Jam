ddd1d6e29e12dea24918fb3d646a20323943777c
Captain Ravioli <thereal.captainravioli@gmail.com>
2021-02-17 16:49:42 +0200

    e

---
+++ Brackeys GameJam 2021.1/Assets/Root/Scripts/PickupAndSnap.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupAndSnap : MonoBehaviour
+{
+    [SerializeField] private Vector2 offset;
+    [SerializeField] private Transform[] slots;
+    private bool isHeld;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isHeld == true)
+        {
+            Vector3 closestPos = Input.mousePosition;
+            foreach (Transform slot in slots)
+            {
+                if (Vector3.Distance(cam.ScreenToWorldPoint(Input.mousePosition), (Vector2)slot.position) < Vector3.Distance(cam.ScreenToWorldPoint(Input.mousePosition), closestPos))
+                {
+                    Debug.Log(slot.name);
+                    closestPos = slot.position;
+                    Debug.Log($"Changed position to {closestPos}");
+                }
+            }
+
+            transform.position = closestPos + (Vector3)offset;
+        }
+
+        Debug.Log(isHeld);
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isHeld = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isHeld = false;
+    }
+}
