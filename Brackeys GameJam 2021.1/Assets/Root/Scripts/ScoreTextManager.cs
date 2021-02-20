3ff199394297217656928c0d20a8e559962da09c
Magna <72097037+MagnaWorks@users.noreply.github.com>
2021-02-20 11:48:19 +0100

    Counting Points cene

---
+++ Brackeys GameJam 2021.1/Assets/Root/Scripts/ScoreTextManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreTextManager : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        scoreText.text = $"Congratulations, you've finished the game! \n You gained a total of {PlayerStats.currentHappiness} happiness of a maximum {PlayerStats.maxHappiness} \n You are the best! ";
+    }
+}
